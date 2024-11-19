907a77780be189d9144f3f9ee0a7f83b1affc7bd
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2024-11-19 22:11:13 +0100

    Initial commit

---
+++ src/main/java/net/thunderstar/quantum_craft/QuantumCraft.java
@@ -0,0 +1,9 @@
+package net.thunderstar.quantum_craft;
+
+import net.minecraftforge.fml.common.Mod;
+import net.thunderstar.quantum_craft.util.Reference;
+
+@Mod(Reference.MOD_ID)
+public class QuantumCraft {
+    
+}
---
+++ src/main/java/net/thunderstar/quantum_craft/util/Reference.java
@@ -0,0 +1,7 @@
+package net.thunderstar.quantum_craft.util;
+
+public class Reference {
+    public static final String MOD_ID = "quantum_craft";
+    public static final String NAME = "Quantum Craft";
+    public static final String VERSION = "0.0.1";
+}
