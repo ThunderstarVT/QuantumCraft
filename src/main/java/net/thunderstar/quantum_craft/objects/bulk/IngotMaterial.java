798ee20640e9c2dea6b519f13bdf132971e7bc6c
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2024-12-16 15:27:26 +0100

    change IngotMaterial to not include ores

--- src/main/java/net/thunderstar/quantum_craft/objects/bulk/IngotMaterial.java
+++ src/main/java/net/thunderstar/quantum_craft/objects/bulk/IngotMaterial.java
@@ -1,19 +1,21 @@
 package net.thunderstar.quantum_craft.objects.bulk;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.thunderstar.quantum_craft.objects.items.TooltipItem;
 
 public class IngotMaterial {
+    public RegistryObject<Item> NUGGET;
     public RegistryObject<Item> INGOT;
-    public RegistryObject<Item> RAW;
+    public RegistryObject<Block> BLOCK;
 
     public IngotMaterial(DeferredRegister<Item> itemRegister, String name) {
-        INGOT = itemRegister.register(name + "_ingot",
-                () -> new TooltipItem(new Item.Properties(), name + "_ingot", 0));
+        NUGGET = itemRegister.register(name + "_nugget",
+                () -> new TooltipItem(new Item.Properties(), name + "_nugget", 0));
 
-        RAW = itemRegister.register("raw_" + name,
-                () -> new TooltipItem(new Item.Properties(), "raw_" + name, 0));
+        INGOT = itemRegister.register(name + "_ingot",
+                () -> new TooltipItem(new Item.Properties(), name + "_ingot", 1));
     }
 }
